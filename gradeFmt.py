94ec6e583a2c8ed1d962515fef4571bb4d33facf
Frederick Robinson <frederick.robinson@gmail.com>
2014-01-28 20:41:02 -0800

    moved from tabs to spaces, refactored to allow non-integer grades

--- gradeFmt.py
+++ gradeFmt.py
@@ -2,104 +2,120 @@
 
 import sys
 
-bars = 15
+bars = 15       # number of bars in summary histogram
+minUID = 999    # value of smallest UID considered as int
+UIDlength = 9   # number of digits to zero-pad UID
+epsilon = .001  # Used for float-int comparison
 
 def isNum(word):
-	try:
-		int(word)
-		return True
-	except:
-		return False
+    try:
+        float(word)
+        return True
+    except:
+        return False
 
 def remDot(fname):
-	loc = fname.rfind('.')
-	if loc == -1:
-		return fname
-	return fname[:loc]
+    loc = fname.rfind('.')
+    if loc == -1:
+        return fname
+    return fname[:loc]
 
 def cnt(lst,a,b):
-	total = 0
-	for x in xrange(a,b+1):
-		# print x, lst.count(x)
-		total += lst.count(x)
-	return total
+    total = 0
+    for pod in lst:
+        if pod <= b+1 and pod >=a:
+            total += 1
+    return total
 
 def mean(seat):
-	tote = 0 
-	for memb in seat:
-		tote += float(memb)
-	return tote / float(len(seat))
+    tote = 0 
+    for memb in seat:
+        tote += float(memb)
+    return tote / float(len(seat))
 
-def intPart(strng):
-	ans = 0
-	real = False
-	for let in strng:
-		if isNum(let):
-			real = True
-			ans*=10
-			ans+=int(let)
+# remove extraneous markup from numbers (i.e. UIDs)
+def sanitize(strang):
+    ans = ""
+    for let in strang:
+        if isNum(let) or let == ".":
+            ans =  ans + let
+    # print "sanitized", strang, "to", ans #debug
+    return ans
 
-	if not real:
-		return -1
-	return ans
+def numbify(str):
+    f = float(str)
+    i = int(f)
+    if abs(f - i) < epsilon:
+        return i
+    return f
 
 def dispRange(a,b):
-	if a==b:
-		return str(a)
-	return str(a)+'-'+str(b)
+    if a==b:
+        return str(a)
+    return str(a)+'-'+str(b)
 
 def main():
-	if len(sys.argv) > 1:
-		inname = sys.argv[1]
-		outname = remDot(inname)+'.tab'
-	else:
-		quit('Please specify .csv file to process.')
+    if len(sys.argv) > 1:
+        inname = sys.argv[1]
+        outname = remDot(inname)+'.tab'
+    else:
+        quit('Please specify .csv file to process.')
 
-	outFile = open(outname,'wb')
-	inFile = open(inname,'rb')
+    outFile = open(outname,'wb')
+    inFile = open(inname,'rb')
 
-	fileContents = inFile.read().splitlines()
-	inFile.close()
+    fileContents = inFile.read().splitlines()
+    inFile.close()
 
-	lines, students = 0,0
-	scores = []
+    lines, students = 0,0
+    scores = []
 
-	for line in fileContents:
-		lines+=1
-		data = line.split(',')
-		toCheck = []
-		for datum in data:
-			if isNum(datum) or (intPart(datum) != -1 and intPart(datum)>=999):	
-				toCheck.append(intPart(datum))
+    for line in fileContents:
+        lines+=1
+        data = line.split(',')
+        toCheck = []
+        for datum in data:
+            clean = sanitize(datum)
+            if isNum(clean):
+                toCheck.append(numbify(clean))
 
-		toCheck.sort()
-		toCheck.reverse()
+        toCheck.sort()
+        toCheck.reverse()
 
-		if len(toCheck)>=2:
-			students+=1
-			uid, grade = toCheck[0],toCheck[1]
-			# print uid, grade
-			if grade > 0 and uid > 999:
-				outFile.write(str(uid)+'\t\t'+str(grade)+'\t\n')
-				scores.append(grade)
+        if len(toCheck)>=2:
+            uid, grade = toCheck[0],toCheck[1]
+            if uid > minUID:
+                students+=1
+                UIDstr = str(uid).zfill(UIDlength)
+                # print UIDstr, "\t", grade, "\t<=\t", line #debug
+                if grade > 0:
+                    outFile.write(UIDstr+'\t\t'+str(grade)+'\t\n')
+                    scores.append(grade)
 
-	outFile.close()
+    outFile.close()
 
-	minS,maxS = min(scores),max(scores)
-	meanS = mean(scores)
+    minS,maxS = int(min(scores)),max(scores)
+    meanS = mean(scores)
 
-	print '%d line read. %d students found.' % (lines, students)
-	print 'Output written to '+outname
+    print '%d line read. %d students found.' % (lines, students)
+    print 'Output written to '+outname
 
-	print '\nStats:'
-	print 'Min: %d\tMax: %d\tMean: %.2f\t' % (minS,maxS,meanS)
-	print ''
+    print '\nStats:'
+    print 'Min: %d\tMax: %d\tMean: %.2f\t' % (minS,maxS,meanS)
+    print ''
 
-	step = (maxS-minS)/bars
-	step = max([step, 1])
+    minRound = int(minS)
+    if abs(maxS - float(int( maxS)) )< epsilon:
+        maxRound = int(maxS)
+    else:
+        maxRound = int(maxS)+1
 
-	for x in xrange(minS,maxS+1,step):
-		print dispRange(x,x+step-1)+'\t'+ '+'* cnt(scores,x,x+step-1)
+
+    step = (maxRound-minRound)/bars
+    step = max([step, 1])
+
+    for x in xrange(minRound,maxRound+1,step):
+        print dispRange(x,x+step-1)+'\t'+ '+'* cnt(scores,x,x+step-1)
 
 
 main()
